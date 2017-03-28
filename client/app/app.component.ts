b90d82dd3607a04645d9b8a0d2e7b31f17d2dd69
Luis Kimura <Luis Kimura>
2017-03-28 06:44:31 -0300

    arrow functions

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -11,6 +11,13 @@ export class AppComponent {
       fotos: Object[]= [];
 
     constructor(http:Http) {
+        
+        http.get('v1/fotos')
+            .map(res => res.json())
+            .subscribe(fotos => {
+                this.fotos = fotos,
+                 erro => console.log(erro);
+            });   
 
     }
 }
