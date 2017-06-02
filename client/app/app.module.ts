8b506d80e9a53225457983bf40c4c6ec8f6d77b5
Luis Kimura <lfkimura@gmail.com>
2017-06-02 17:57:56 -0300

    form validator

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -10,12 +10,20 @@ import { CadastroComponent }   from './cadastro/cadastro.component'; // importou
 import { ListagemComponent }   from './listagem/listagem.component'; // importou
 
 // Importa o módulo. Não esqueça de adicioná-lo no array de imports!
-import { FormsModule } from '@angular/forms';
 import { routing } from './app.routes';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  imports:      [ BrowserModule, FotoModule, HttpModule, PainelModule, routing, FormsModule ],
-  declarations: [ AppComponent, CadastroComponent, ListagemComponent ],
+  imports:      [ 
+    BrowserModule, 
+    HttpModule, 
+    PainelModule, 
+    FotoModule,
+    routing,
+    FormsModule, 
+    ReactiveFormsModule
+  ],
+  declarations: [ AppComponent, ListagemComponent, CadastroComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
