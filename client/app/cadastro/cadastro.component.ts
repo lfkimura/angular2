03e014e87b3b6d9d7e41cd760cad5a0714d7f39a
Luis Kimura <lfkimura@gmail.com>
2017-06-06 07:50:02 -0300

    alteração de fotos

--- client/app/cadastro/cadastro.component.ts
+++ client/app/cadastro/cadastro.component.ts
@@ -2,6 +2,7 @@ import {Component, Input} from '@angular/core';
 import {FotoComponent} from '../foto/foto.component';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { FotoService } from '../foto/foto.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
     moduleId: module.id,
@@ -14,10 +15,25 @@ export class CadastroComponent {
     foto: FotoComponent = new FotoComponent();
     service: FotoService;
     meuForm: FormGroup;
+    route: ActivatedRoute;
+    mensagem: string ='';
+    router: Router;
 
-    constructor(service: FotoService, fb: FormBuilder) {
+    constructor(service: FotoService, fb: FormBuilder, route: ActivatedRoute, router: Router) {
 
+        this.route = route;
+        this.router = router;
         this.service = service;
+        this.route.params.subscribe(params =>{
+            let id = params['id'];
+
+            if(id) {
+
+                this.service.buscaPorId(id)
+                    .subscribe(
+                        foto => this.foto = foto,
+                        erro => console.log(erro));    
+            }} );
 
         this.meuForm = fb.group({
             titulo: ['', Validators.compose([Validators.required, Validators.minLength(4)])],
@@ -34,6 +50,7 @@ export class CadastroComponent {
                 console.log(x);
                 this.foto = new FotoComponent();
                 console.log('Foto salva com sucesso');
+                this.router.navigate(['']);
             }, erro =>  console.log(erro));
         console.log(this.foto);
     }
